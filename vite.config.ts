5533fe54bc4d43b5d9a49b61f3fe4f940b81c404
Ren <renzotrigoso@gmail.com>
2024-08-30 15:19:26 -0400

    first changes to setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    },
+  },
   plugins: [preact()],
 })
