18c4a212ddf3dc2d07a0db1b12bce32fa9a92036
Ren <renzotrigoso@gmail.com>
2024-08-31 14:26:51 -0400

    improve UI on smaller screens. add small UI flare on page load for fun.

--- src/components/ChordDisplay.tsx
+++ src/components/ChordDisplay.tsx
@@ -1,5 +1,5 @@
 import './ChordDisplay.css';
-import { ChordName, Note, Notes } from "@/constants/music";
+import { CHORDS, ChordName, Note, Notes } from "@/constants/music";
 import { GetNotesInChord } from "@/utils/music";
 import { Signal } from "@preact/signals";
 import { Component, Fragment } from "preact";
@@ -9,21 +9,51 @@ interface ChordDisplayProps {
     rootNote: Signal<Note>;
 };
 
-class ChordDisplay extends Component<ChordDisplayProps> {
+interface ChordDisplayState {
+    isInSemitones: boolean; 
+    isLoading: boolean;
+}
+
+class ChordDisplay extends Component<ChordDisplayProps, ChordDisplayState> {
     constructor(props: ChordDisplayProps) {
         super(props);
     }
 
+    onHover () {
+        this.setState({ isInSemitones: true });
+    }
+
+    onDrop () {
+        this.setState({ isInSemitones: false });
+    }
+
+    refreshDisplay () {
+        const randomTimeout = 100 * Math.floor(Math.random() * 10);
+        this.setState({ isLoading: true });
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, randomTimeout);
+    }
+
+    componentDidMount () {
+        this.refreshDisplay();
+    }
+
     render () {
+        const { isInSemitones, isLoading } = this.state;
         const { rootNote, name } = this.props;
-        const notes: Notes = GetNotesInChord(rootNote.value, name)
+        const notes: Notes = GetNotesInChord(rootNote.value, name);
 
         return (
-            <div class="chord-display" >
+            <div class={`chord-display ${isLoading ? 'is-loading' : ''}`} >
                 <div class="chord-display-name">{name}</div>
-                <div class="chord-display-notes">
+                <div 
+                    class="chord-display-notes" 
+                    onMouseEnter={this.onHover.bind(this)}
+                    onMouseLeave={this.onDrop.bind(this)}
+                >
                 {
-                    notes.map(note => (
+                    (isInSemitones ? CHORDS[name] : notes).map(note => (
                         <Fragment key={note}>
                             <span class="chord-display-note">{note}</span>
                         </Fragment>
