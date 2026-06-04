70cea6ca57d242ab3d7547ecc5c21e990797b60a
rtrigoso <renzotrigoso@gmail.com>
2026-06-04 10:22:26 -0400

    avoid creating a new instance of the signal on every render

--- src/composites/chord-calculator.tsx
+++ src/composites/chord-calculator.tsx
@@ -1,4 +1,4 @@
-import { signal } from "@preact/signals";
+import { useSignal } from "@preact/signals";
 import { CHORD_NAMES, Note, BALANCE_MAP, Balance } from '../constants/music';
 import { NoteSelector } from '../components/NoteSelector';
 import { ChordDisplay } from '../components/ChordDisplay';
@@ -6,8 +6,8 @@ import { Fragment } from 'preact/jsx-runtime';
 import { BalanceSelector } from '../components/BalanceSelector';
 
 export default function ChordCalculator() {
-  const rootNote = signal<Note>("C");
-  const balance = signal<Balance>(BALANCE_MAP[3]);
+  const rootNote = useSignal<Note>("C");
+  const balance = useSignal<Balance>(BALANCE_MAP[3]);
 
   return (
     <div class="flex flex-col m-2 gap-2 lg:w-1/3 w-full">
